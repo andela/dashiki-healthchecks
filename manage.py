3918f6f65f711a9b5a4430d1567d8113e9f5bf21
Sebuuma Daniel <sedzsoft@gmail.com>
2017-06-13 20:28:52 +0300

    Add flake8 config, pre-commit-example file and enforces it when the server is run.

--- manage.py
+++ manage.py
@@ -2,7 +2,13 @@
 import os
 import sys
 
+
+def add_pre_commit():
+    os.system("cp pre-commit-example .git/hooks/pre-commit")
+
 if __name__ == "__main__":
+    add_pre_commit()  # Add pre-commit configurations
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hc.settings")
 
     from django.core.management import execute_from_command_line
