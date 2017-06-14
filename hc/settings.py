5810f1dc0ce5433a07ada5be1d393d6fc928589e
Donna Mwiine <donna@Donnas-MacBook-Pro.local>
2017-06-14 16:19:14 +0300

    Add production and staging settings for heroku

--- hc/settings.py
+++ hc/settings.py
@@ -128,6 +128,14 @@ if os.environ.get("DB") == "mysql":
             'TEST': {'CHARSET': 'UTF8'}
         }
     }
+if os.environ.get("ENVIRONMENT") == "staging":
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
+
+if os.environ.get("ENVIRONMENT") == "production":
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
+
 
 if not DEBUG:
     db_from_env = dj_database_url.config(conn_max_age=500)
