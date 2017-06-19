b6c8b649cbd55580af5be1afe669aabcb5cf1a35
Sebuuma Daniel <sedzsoft@gmail.com>
2017-06-19 14:33:59 +0300

    Use staging and production database setting if DEBUG is set to 'False'

--- hc/settings.py
+++ hc/settings.py
@@ -111,7 +111,7 @@ if os.environ.get("DB") == "mysql":
         }
     }
 
-if DEBUG:
+if not DEBUG:
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
