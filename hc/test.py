77c16b4e1794c595d55c7c07cbcf6838753a3a01
Sebuuma Daniel <sedzsoft@gmail.com>
2017-06-20 21:13:07 +0300

    [#147170289] Add more dummy data to the BaseTestCase test.py file

--- hc/test.py
+++ hc/test.py
@@ -35,4 +35,7 @@ class BaseTestCase(TestCase):
         self.charlie.set_password("password")
         self.charlie.save()
 
-        ### Set Charles not to have access to Alice's stuff
+        # Set Charles not to have access to Alice's stuff
+        self.charlie_profile = Profile(user=self.charlie, api_key='def')
+        self.charlie_profile.team_access_allowed = True
+        self.charlie_profile.save()
