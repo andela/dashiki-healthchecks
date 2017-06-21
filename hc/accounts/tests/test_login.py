88be60e3e4f18e198ec42a58d627322fee4a9b2a
Donna Mwiine <donna@Donnas-MacBook-Pro.local>
2017-06-21 13:02:30 +0300

    [#147170291] Fixed login test case tests

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -14,19 +14,30 @@ class LoginTestCase(TestCase):
         session["welcome_code"] = str(check.code)
         session.save()
 
+        # Get initial user count
+        initial_count = len(User.objects.all())
+
         form = {"email": "alice@example.org"}
 
         r = self.client.post("/accounts/login/", form)
         assert r.status_code == 302
 
-        ### Assert that a user was created
+        # Assert that a user was created
+        final_count = len(User.objects.all())
+
+        # Assert for user count increment
+        self.assertEqual(final_count - initial_count, 1)
+
 
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
-        ### Assert contents of the email body
 
-        ### Assert that check is associated with the new user
+        # Assert contents of the email body
+        self.assertTrue(len(mail.outbox[0].body) > 0)
+
+        # Assert that check is associated with the new user
+        self.assertTrue(check.user is None)
 
     def test_it_pops_bad_link_from_session(self):
         self.client.session["bad_link"] = True
