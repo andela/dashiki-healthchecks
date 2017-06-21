4b0d5032b8cf88017e2e9835884deb19f6288796
Collins Abitekaniza <abtcolns@gmail.com>
2017-06-21 12:18:30 +0300

    [#147170291] Add check token test case tests

--- hc/accounts/tests/test_check_token.py
+++ hc/accounts/tests/test_check_token.py
@@ -1,5 +1,8 @@
-from django.contrib.auth.hashers import make_password
 from hc.test import BaseTestCase
+from django.contrib.auth.hashers import make_password
+from django.urls import reverse
+from hc.accounts.models import User, Profile
+import uuid
 
 
 class CheckTokenTestCase(BaseTestCase):
@@ -21,8 +24,37 @@ class CheckTokenTestCase(BaseTestCase):
         self.profile.refresh_from_db()
         self.assertEqual(self.profile.token, "")
 
-    ### Login and test it redirects already logged in
+    # Login and test it redirects already logged in
+    def test_login_redirects(self):
+        # Create test user
+        user = User(username="test", email="test@gmail.com")
+        user.set_password("password123")
+        user.save()
 
-    ### Login with a bad token and check that it redirects
+        # Login test user with valid credentials
+        self.client.login(username="test@gmail.com", password="password123")
+        response = self.client.get("/")
+        self.assertRedirects(response, "/checks/")
 
-    ### Any other tests?
+    # Login with a bad token and check that it redirects
+    def test_login_with_bad_token(self):
+        user = User(username='test', email='test@gmail.com')
+        user.set_password("password123")
+        user.save()
+
+        # Create profile instance
+        profile = Profile(user=user)
+        correct_token, wrong_token = str(uuid.uuid4()), str(uuid.uuid4())
+        profile.token = make_password(correct_token)
+
+        params = {
+            "username": user.username,
+            "token": wrong_token
+        }
+
+        uri = reverse("hc-check-token", args=[user.username, wrong_token])
+
+        # Try logging in with wrong token
+        response = self.client.post(uri, params)
+        self.assertRedirects(response, '/accounts/login/')
+    # Any other tests?
