12e3f8a3231e18657bc0c79d8f411f405fc2f84c
charlieoduk <odukjr@gmail.com>
2017-06-21 16:04:15 +0300

    [#147170289] Update badge test for bad signature. Test svg is returned

--- hc/api/tests/test_badge.py
+++ hc/api/tests/test_badge.py
@@ -12,13 +12,16 @@ class BadgeTestCase(BaseTestCase):
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
 
     def test_it_rejects_bad_signature(self):
-        r = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
-        ### Assert the expected response status code
+        response = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
+        # Assert the expected response status code
+        self.assertEqual(response.status_code, 400)
 
     def test_it_returns_svg(self):
-        sig = base64_hmac(str(self.alice.username), "foo", settings.SECRET_KEY)
-        sig = sig[:8].decode("utf-8")
-        url = "/badge/%s/%s/foo.svg" % (self.alice.username, sig)
+        signature = base64_hmac(str(self.alice.username), "foo", settings.SECRET_KEY)
+        signature = signature[:8].decode("utf-8")
+        url = "/badge/%s/%s/foo.svg" % (self.alice.username, signature)
 
-        r = self.client.get(url)
-        ### Assert that the svg is returned
+        response = self.client.get(url)
+        # Assert that the svg is returned
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('foo', response.content.decode('utf-8'))
