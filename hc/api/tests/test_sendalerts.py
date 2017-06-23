cb4675f042fc980ac610c0302a765b725a74f287
Sebuuma Daniel <sedzsoft@gmail.com>
2017-06-23 19:10:53 +0300

    [#147170289] Test sendalert functionality can send many or one alert.

--- hc/api/tests/test_sendalerts.py
+++ hc/api/tests/test_sendalerts.py
@@ -28,7 +28,7 @@ class SendAlertsTestCase(BaseTestCase):
             handled_names.append(args[0].name)
 
         assert set(names) == set(handled_names)
-        ### The above assert fails. Make it pass
+        # The above assert fails. Make it pass
 
     def test_it_handles_grace_period(self):
         check = Check(user=self.alice, status="up")
@@ -37,6 +37,20 @@ class SendAlertsTestCase(BaseTestCase):
         check.save()
 
         # Expect no exceptions--
-        Command().handle_one(check)
+        result = Command().handle_one(check)
+        assert result, "handle_one should return True"
 
-    ### Assert when Command's handle many that when handle_many should return True
+    # Assert when Command's handle many, handle_many should return True
+    @patch("hc.api.management.commands.sendalerts.Command.handle_one")
+    def test_handles_many_should_return_true(self, mock):
+        yesterday = timezone.now() - timedelta(days=1)
+        names = ["Check %d" % d for d in range(0, 30)]
+
+        for name in names:
+            check = Check(user=self.alice, name=name)
+            check.alert_after = yesterday
+            check.status = "up"
+            check.save()
+
+        result = Command().handle_many()
+        assert result, "handle_many should return True"
