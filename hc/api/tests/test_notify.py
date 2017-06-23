9171c0f23b7fe1991fd265a3971ad5114621fb3f
Sebuuma Daniel <sedzsoft@gmail.com>
2017-06-23 16:25:48 +0300

    [#147170289] Tests that nofications respond to 500 error and ConnectionError.

--- hc/api/tests/test_notify.py
+++ hc/api/tests/test_notify.py
@@ -4,7 +4,7 @@ from django.core import mail
 from django.test import override_settings
 from hc.api.models import Channel, Check, Notification
 from hc.test import BaseTestCase
-from mock import patch
+from mock import patch, Mock
 from requests.exceptions import ConnectionError, Timeout
 
 
@@ -222,4 +222,15 @@ class NotifyTestCase(BaseTestCase):
         json = kwargs["json"]
         self.assertEqual(json["message_type"], "CRITICAL")
 
-    ### Test that the web hooks handle connection errors and error 500s
+    # Test that the web hooks handle connection errors and error 500s
+    @patch("hc.api.transports.requests.request")
+    def test_web_hook_handles_connection_errors_and_500_error(self, mock_get):
+        self._setup_data("webhook", "http://example")
+        mock_get.return_value.status_code = 500
+        self.channel.notify(self.check)
+
+        n = Notification.objects.get()
+        self.assertEqual(n.error, "Received status code 500")
+
+        self.channel.notify = Mock(side_effect=ConnectionError)
+        self.assertRaises(ConnectionError, self.channel.notify, self.check)
