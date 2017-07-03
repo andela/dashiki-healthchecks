a6de19437cb407cf668236d31170aad827260ea4
Collins Abitekaniza <abtcolns@gmail.com>
2017-07-03 17:35:47 +0300

    [#147170309] Fix empty form submit, add spacing between media content

--- hc/help_videos/views.py
+++ hc/help_videos/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from .models import Video
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -8,3 +12,24 @@ def index(request):
         'section': "help-videos"
     }
     return render(request, 'help_videos/videos.html', ctx)
+
+
+@login_required
+def upload(request):
+    if request.method == 'POST':
+        title, desc = request.POST['title'], request.POST['description']
+        video = request.FILES['video-file']
+
+        if len(title) < 1 or len(desc) < 1:
+            return HttpResponse("Please fill all fields")
+
+        fstorage = FileSystemStorage()
+        fname = fstorage.save(video.name, video)
+        upload_uri = fstorage.url(fname)
+
+        # Add entry to database
+        video = Video(title=title, description=desc, resource_url=upload_uri)
+        video.save()
+        return HttpResponse("success")
+    else:
+        return HttpResponse("failed")
