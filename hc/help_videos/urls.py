12c993775906238023f60cc7c8aa75364756cb15
Collins Abitekaniza <abtcolns@gmail.com>
2017-07-03 14:43:39 +0300

    [#147170309] Add Ajax file upload for help videos

--- hc/help_videos/urls.py
+++ hc/help_videos/urls.py
@@ -9,5 +9,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='hc-help-videos')
+    url(r'^$', views.index, name='hc-help-videos'),
+    url(r'^upload$', views.upload, name="hc-help-videos-upload")
 ]
