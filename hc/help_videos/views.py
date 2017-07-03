10ee95e0fadb6a4d13b94e893698461e17d4c626
Collins Abitekaniza <abtcolns@gmail.com>
2017-07-03 15:59:57 +0300

    [#147170309] Add media upload backend functionality

--- hc/help_videos/views.py
+++ hc/help_videos/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from .models import Video
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -12,5 +15,16 @@ def index(request):
 
 def upload(request):
     if request.method == 'POST':
-        pass
-    pass
+        title, desc = request.POST['title'], request.POST['description']
+        video = request.FILES['video-file']
+
+        fstorage = FileSystemStorage()
+        fname = fstorage.save(video.name, video)
+        upload_uri = fstorage.url(fname)
+
+        # Add entry to database
+        video = Video(title=title, description=desc, resource_url=upload_uri)
+        video.save()
+        return HttpResponse("success")
+    else:
+        return HttpResponse("failed")
