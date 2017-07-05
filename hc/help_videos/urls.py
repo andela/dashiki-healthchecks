f852698b51a17da26eb3dd10c524e37549773061
Collins Abitekaniza <abtcolns@gmail.com>
2017-07-05 15:28:00 +0300

    Add delete video functionality

--- hc/help_videos/urls.py
+++ hc/help_videos/urls.py
@@ -10,5 +10,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='hc-help-videos'),
-    url(r'^upload$', views.upload, name="hc-help-videos-upload")
+    url(r'^upload$', views.upload, name="hc-help-videos-upload"),
+    url(r'^delete$', views.delete_video, name="hc-delete-video")
 ]
