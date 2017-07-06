d1610d7982445558321bba1b3211558fb651073a
Seremba John <john.seremba@andela.com>
2017-07-06 15:46:58 +0300

    [#147170309] Add FAQ Tests

---
+++ hc/front/tests/test_add_faq.py
@@ -0,0 +1,63 @@
+from hc.front.models import (FaqItem, FaqCategory)
+from hc.front.forms import (AddFaqCategoryForm, AddFaqForm)
+from hc.test import BaseTestCase
+from django.urls import reverse
+
+
+class AddFaqTestCase(BaseTestCase):
+
+    def test_create_faq_cat(self):
+        self.client.login(username="admin", password="pass")
+        form = AddFaqCategoryForm({'category': 'Category One'})
+        self.assertTrue(form.is_valid())
+        result = form.save()
+        self.assertEqual(result.category, "Category One")
+
+    def test_create_faq(self):
+        category = FaqCategory.objects.create(category='Category One')
+        faq = FaqItem.objects.create(title='FAQ Title', body='FAQ Body', category=category)
+        self.assertTrue(faq)
+        self.assertEqual(faq.title, 'FAQ Title')
+        self.assertEqual(faq.body, 'FAQ Body')
+        self.assertEqual(category.category, 'Category One')
+
+    def test_submit_empty_forms(self):
+        category_form = AddFaqCategoryForm({})
+        faq_item_form = AddFaqForm({})
+        self.assertFalse(category_form.is_valid())
+        self.assertFalse(faq_item_form.is_valid())
+
+    def test_edit_faq(self):
+        self.client.login(username="admin", password="pass")
+        category = FaqCategory.objects.create(category='Category One')
+        faq = FaqItem.objects.create(title='FAQ Title', body='FAQ Body', category=category)
+        response = self.client.get(reverse("hc-faq-edit", kwargs={'id': faq.id}))
+        self.assertContains(response, "FAQ Body", status_code=200)
+
+    def test_access_to_faq_creation(self):
+        response = self.client.get(reverse("hc-save-faq"))
+        self.assertContains(response, "", status_code=302)
+        self.assertIn("/accounts/login/", response.url)
+
+    def test_delete_faq(self):
+        self.client.login(username="admin", password="pass")
+        category = FaqCategory.objects.create(category='Category One')
+        faq = FaqItem.objects.create(title='FAQ Title', body='FAQ Body', category=category)
+        FaqItem.objects.create(title='FAQ Title2', body='FAQ Body2', category=category)
+        response = self.client.get(reverse("hc-faq-delete", kwargs={'id': faq.id}))
+        count = FaqItem.objects.count()
+        self.assertContains(response, "", status_code=302)
+        self.assertEqual(count, 1)
+
+    def test_delete_faq_category(self):
+        self.client.login(username="admin", password="pass")
+        category = FaqCategory.objects.create(category='Category One')
+        FaqItem.objects.create(title='FAQ Title', body='FAQ Body', category=category)
+        FaqItem.objects.create(title='FAQ Title2', body='FAQ Body2', category=category)
+        response = self.client.get(reverse("hc-cat-delete", kwargs={'id': category.id}))
+        count_cat = FaqCategory.objects.count()
+        count_items = FaqItem.objects.count()
+        print("Cat: " + str(count_cat) + " Items: " + str(count_items))
+        self.assertContains(response, "", status_code=302)
+        self.assertEqual(count_cat, 0)
+        self.assertEqual(count_items, 0)
