b2d2a5c650edfc6a18d65a72828ac44165413030
Sebuuma Daniel <sedzsoft@gmail.com>
2017-07-10 22:30:44 +0300

    [#147170313] Test priority object updates status to 'contacted' when notification are sent and is changed to 'pending' when check changes to up.

--- hc/front/tests/test_priority_list.py
+++ hc/front/tests/test_priority_list.py
@@ -1,3 +1,9 @@
+from datetime import timedelta
+
+from django.utils import timezone
+from mock import patch
+
+from hc.api.management.commands.sendalerts import Command
 from hc.test import BaseTestCase
 
 from hc.api.models import Check, Priority
@@ -29,6 +35,24 @@ class PriorityListTestCase(BaseTestCase):
         self.assertRedirects(response, "/checks/", 302)
         self.assertEqual(Priority.objects.count(), 1)
 
+    @patch("hc.api.management.commands.sendalerts.Command.handle_one")
+    def test_priority_list_is_used_to_send_notifications(self, mock_handle):
+        now = timezone.now()
+        yesterday = now - timedelta(days=1)
+
+        self.check.alert_after = yesterday
+        self.check.last_ping = now - timedelta(days=1, minutes=30)
+        self.check.status = "up"
+        self.check.save()
+        self.check.refresh_from_db()
+
+        priority = Priority(user=self.alice, current_check=self.check, level=PRIORITY_LEVELS_DICT.get("high"))
+        priority.save()
+
+        result = Command().handle_one(self.check)
+
+        assert result, "handle_one should return True"
+
     def test_update_of_priority_object_for_a_user(self):
         priority = Priority(user=self.alice, current_check=self.check, level=PRIORITY_LEVELS_DICT.get("high"))
         priority.save()
@@ -38,3 +62,17 @@ class PriorityListTestCase(BaseTestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Priority.objects.first().level, PRIORITY_LEVELS_DICT.get("medium"))
+
+    def test_priority_objects_for_a_check_are_updated_back_to_pending_when_check_is_pinged(self):
+        self.check.status = "down"
+        self.check.save()
+        self.check.refresh_from_db()
+
+        priority = Priority(user=self.alice, current_check=self.check,
+                            level=PRIORITY_LEVELS_DICT.get("high"),
+                            status="contacted")
+        priority.save()
+
+        self.client.get("/ping/{}".format(self.check.code))
+
+        self.assertEqual(Priority.objects.first().status, "pending")
