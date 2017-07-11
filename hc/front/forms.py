45c9683b37913dfb1358adaed27519269028fe69
Donna Mwiine <donna.mwiine@andela.com>
2017-07-11 18:39:27 +0300

    [#147170295] Fix pylint errors

--- hc/front/forms.py
+++ hc/front/forms.py
@@ -24,8 +24,10 @@ class TimeoutForm(forms.Form):
     timeout = forms.IntegerField(min_value=60, max_value=2592000)
     grace = forms.IntegerField(min_value=60, max_value=2592000)
 
+
 class NagTimeForm(forms.Form):
     nag_time = forms.IntegerField(min_value=60, max_value=2592000)
+
 
 class AddChannelForm(forms.ModelForm):
 
