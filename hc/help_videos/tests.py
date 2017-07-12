3c21eee5c95fdfb8bf6c1310987c6bbfbe2ed171
Collins Abitekaniza <abtcolns@gmail.com>
2017-07-12 13:05:55 +0300

    [#147170309] Remove files used during tests

--- hc/help_videos/tests.py
+++ hc/help_videos/tests.py
@@ -76,3 +76,5 @@ class HelpVideosTestCase(TestCase):
         self.client.logout()
         os.system("rm -r {}".format("{}{}".format(BASE_DIR,
                                                   "/dummy.mp4").replace("%20", "\ ")))
+        os.system("rm -r {}{}".format(BASE_DIR,
+                                      "/media/dummy.mp4".replace("%20", "\ ")))
