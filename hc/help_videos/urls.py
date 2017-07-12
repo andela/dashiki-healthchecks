5c1938c7ae48a45fa772cc92e98b74d5b6a10a1d
Collins Abitekaniza <abtcolns@gmail.com>
2017-07-12 09:33:41 +0300

    [#147170309] Remove editor headers from urls.py

--- hc/help_videos/urls.py
+++ hc/help_videos/urls.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# File: urls.py
-# Author: Collins Abitekaniza <abtcolns@gmail.com>
-# Date: 03.07.2017
-# Last Modified: 03.07.2017
-
 from django.conf.urls import url
 from . import views
 
