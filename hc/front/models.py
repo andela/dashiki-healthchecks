6d0d20aa0e33eff00472e91626a25bd71de7a2a4
Sebuuma Daniel <sedzsoft@gmail.com>
2017-07-14 21:15:18 +0300

    [#147170315] Add new Post model and accompanying migrations.

--- hc/front/models.py
+++ hc/front/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from hc.front.time_stamped_model import TimeStampedModel
+
+
+class Post(TimeStampedModel):
+    user = models.ForeignKey(User, blank=True, null=False, related_name="posts")
+    title = models.CharField(max_length=256, null=False)
+    body = models.TextField(null=True)
+    slug = models.CharField(max_length=400, null=False, unique=True)
+    publish = models.BooleanField(default=False)
+
+    def save(self, **kwargs):
+        self.slug = self._generate_slug(self.title)
+        return super(Post, self).save(**kwargs)
+
+    def _generate_slug(self, title):
+        slug = "-".join([str(token) for token in title.lower().split(" ")])
+        if Post.objects.filter(slug=slug).all():
+            slug = slug + "-" + str(Post.objects.last().id + 1)
+        return slug
+
