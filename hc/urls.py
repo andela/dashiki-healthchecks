400a2ce2dc87ec2b6ebf9ed46cec43d72d84c174
Collins Abitekaniza <abtcolns@gmail.com>
2017-07-20 13:15:11 +0300

    [#147170309] Fix show video bug

--- hc/urls.py
+++ hc/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from hc import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -7,4 +9,4 @@ urlpatterns = [
     url(r'^', include('hc.api.urls')),
     url(r'^', include('hc.front.urls')),
     url(r'^', include('hc.payments.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
